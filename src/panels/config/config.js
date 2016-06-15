6adaf123ec05aa42f98d25fae3a487cdaed43b43
Artur Arseniev <artur.catch@hotmail.it>
2016-06-15 15:32:34 +0200

    Add default panels

--- src/panels/config/config.js
+++ src/panels/config/config.js
@@ -1,14 +1,65 @@
 define(function () {
+	var slc = 'select-comp';
+	var crc = 'create-comp';
+	var mvc = 'move-comp';
+	var txc = 'text-comp';
+	var imc = 'image-comp';
+	var swv = 'sw-visibility';
+	var expt = 'export-template';
+
 	return {
-		stylePrefix		: 'pn-',
-		
+		stylePrefix: 'pn-',
+
 		// Default panels
-		defaults		: [],
-		
+		defaults: [{
+			id: 'commands',
+			buttons: [{
+				id: slc,
+				command: slc,
+				className: 'fa fa-mouse-pointer',
+				attributes: { title	: 'Select element' },
+			},{
+				id: crc,
+				command: crc,
+				className: 'fa fa-plus-square-o',
+				attributes: { title	: 'Create element' },
+			},{
+				id: mvc,
+				command: mvc,
+				className: 'fa fa-arrows',
+				attributes: { title: 'Move elements' },
+			},{
+				id: txc,
+				command: txc,
+				className: 'fa fa-font',
+				attributes: { title: 'Create text element' },
+			},{
+				id: imc,
+				command: imc,
+				className: 'fa fa-picture-o',
+				attributes: { title: 'Create image element' },
+			}],
+		},{
+			id: 'options',
+			buttons: [{
+				active: true,
+				id: swv,
+				className: 'fa fa-eye',
+				command: swv,
+				context: swv,
+				attributes: { title: 'View components' },
+			},{
+				id: expt,
+				className: 'fa fa-code',
+				command: expt,
+				attributes: { title: 'View code' },
+			}],
+		}],
+
 		// Editor model
-		em				: null,
-		
+		em : null,
+
 		// Delay before show children buttons (in milliseconds)
-		delayBtnsShow	: 300,			
+		delayBtnsShow	: 300,
 	};
 });
