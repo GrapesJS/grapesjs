e4c97bf01986f129682a7856e41baaa6873fee19
Artur Arseniev <artur.catch@hotmail.it>
2016-09-30 14:59:59 +0200

    Update traits

--- src/trait_manager/view/TraitView.js
+++ src/trait_manager/view/TraitView.js
@@ -3,10 +3,12 @@ define(['backbone'], function (Backbone) {
 	return Backbone.View.extend({
 
 		events:{
-			'change': 'change'
+			'change': 'onChange'
 		},
 
 		initialize: function(o) {
+			this.config = o.config || {};
+			this.pfx = this.config.stylePrefix || '';
 		},
 
 		/**
@@ -21,7 +23,23 @@ define(['backbone'], function (Backbone) {
 			elView.model.set('attributes', attrs);
 		},
 
+		/**
+		 * Render label
+		 * @private
+		 */
+		renderLabel: function() {
+			this.$el.html(this.templateLabel({
+				pfx		: this.pfx,
+				ppfx	: this.ppfx,
+				icon	: this.model.get('icon'),
+				info	: this.model.get('info'),
+				label	: this.model.get('name'),
+			}));
+		},
+
 		render : function() {
+			this.renderLabel();
+			this.renderField();
 			return this;
 		},
 
