ac84425ab4fb55c6069f9a50425bdeb1cef08ce3
Artur Arseniev <artur.catch@hotmail.it>
2017-01-28 11:28:29 +0100

    0.3.50

--- src/demo.js
+++ src/demo.js
@@ -329,12 +329,6 @@ require(['config/require-config'], function() {
 
     window.editor = editor;
 
-		$(document).ready(function(){
-			console.log('doc ready4');
-		});
-
-		$('.gjs-cv-canvas').css('padding-left', '200px');
-
 		/*
 		// Test toolbar commands
 		var cmd = editor.Commands;
