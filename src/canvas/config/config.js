cff7984fefc83d45cd043b760a18a7d98cc84cf7
tangkikodo <allmonday@126.com>
2017-04-13 17:08:28 +0800

    update comment of scripts

--- src/canvas/config/config.js
+++ src/canvas/config/config.js
@@ -6,7 +6,10 @@ define(function () {
 		// Coming soon
 		rulers: false,
 
-		// append scripts in head
+		/*
+		 * append scripts in head of iframe before renderBody content
+		 * need to manually maintain the same scripts in cms's render template
+		*/ 
 		scripts: []
 
 	};
