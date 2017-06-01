008c08018b980e3bf3e63a803a1bfe2955448909
Artur Arseniev <artur.catch@hotmail.it>
2017-06-02 00:23:54 +0200

    Lebab no-strict

--- src/device_manager/model/Devices.js
+++ src/device_manager/model/Devices.js
@@ -1,11 +1,6 @@
-define((require, exports, module) => {
-  'use strict';
-  var Backbone = require('backbone');
-  var Device = require('./Device');
+var Backbone = require('backbone');
+var Device = require('./Device');
 
-    module.exports = Backbone.Collection.extend({
-
-      model:  Device,
-
-    });
+module.exports = Backbone.Collection.extend({
+  model:  Device,
 });
