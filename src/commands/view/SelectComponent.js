501795d0c2161efa415be7750ab554964e9a6492
Artur Arseniev <artur.catch@hotmail.it>
2017-07-14 13:29:32 +0200

    Make use of getName in SelectComponent command

--- src/commands/view/SelectComponent.js
+++ src/commands/view/SelectComponent.js
@@ -250,7 +250,7 @@ module.exports = {
     if(!model || !model.get('badgable'))
       return;
     var badge = this.getBadge();
-    badge.innerHTML = model.getCurrentName();
+    badge.innerHTML = model.getName();
     var bStyle = badge.style;
     var u = 'px';
     bStyle.display = 'block';
