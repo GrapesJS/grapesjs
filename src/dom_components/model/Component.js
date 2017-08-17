c45e572ca32173b52c9d634af5816c0290f59268
Artur Arseniev <artur.catch@hotmail.it>
2017-08-17 20:24:02 +0200

    Fix few regressions after the trait update

--- src/dom_components/model/Component.js
+++ src/dom_components/model/Component.js
@@ -161,7 +161,10 @@ module.exports = Backbone.Model.extend(Styleable).extend({
     this.get('traits').each((trait) => {
       found = 1;
       if (!trait.get('changeProp')) {
-        attrs[trait.get('name')] = trait.getInitValue();
+        const value = trait.getInitValue();
+        if (value) {
+          attrs[trait.get('name')] = value;
+        }
       }
     });
 
