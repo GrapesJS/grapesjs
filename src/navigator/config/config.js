1abcfd2ef5d4fb4b22adb8d4b136aac76b6c97ff
Artur Arseniev <artur.catch@hotmail.it>
2017-08-22 03:05:35 +0200

    Update layers configs

--- src/navigator/config/config.js
+++ src/navigator/config/config.js
@@ -1,10 +1,14 @@
 module.exports = {
   stylePrefix: 'nv-',
+
+  // Enable/Disable globally the possibility to sort layers
   sortable: 1,
+
+  // Enable/Disable globally the possibility to hide layers
   hidable: 1,
+
+  // Hide textnodes
   hideTextnode: 1,
-  itemClass: 'item',
-  itemsClass: 'items',
 
   // Indicates if the wrapper is visible in layers
   showWrapper: 1,
