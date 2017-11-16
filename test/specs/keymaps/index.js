450fe01ccc8b7ae921dc2c8ce1bfbedcce270c45
Artur Arseniev <artur.catch@hotmail.it>
2017-11-16 01:49:15 +0100

    Start with keymaps tests

---
+++ test/specs/keymaps/index.js
@@ -0,0 +1,61 @@
+const Editor = require('editor/model/Editor');
+const Keymaps = require('keymaps');
+
+describe.only('Keymaps', () => {
+
+  describe('Main', () => {
+
+    let em;
+    let obj;
+
+    beforeEach(() => {
+      em = new Editor();
+      obj = new Keymaps().init({em});
+    });
+
+    it('Object exists', () => {
+      expect(obj).toExist();
+    });
+
+    it('No device inside', () => {
+      var coll = obj.getAll();
+      expect(coll.length).toEqual(0);
+    });
+
+    it('Add new device', () => {
+      var model = obj.add(testNameDevice, testWidthDevice);
+      expect(obj.getAll().length).toEqual(1);
+    });
+
+    it('Added device has correct data', () => {
+      var model = obj.add(testNameDevice, testWidthDevice);
+      expect(model.get('name')).toEqual(testNameDevice);
+      expect(model.get('width')).toEqual(testWidthDevice);
+    });
+
+    it('Add device width options', () => {
+      var model = obj.add(testNameDevice, testWidthDevice, {opt: 'value'});
+      expect(model.get('opt')).toEqual('value');
+    });
+
+    it('The name of the device is unique', () => {
+      var model = obj.add(testNameDevice, testWidthDevice);
+      var model2 = obj.add(testNameDevice, '2px');
+      expect(model).toEqual(model2);
+    });
+
+    it('Get device by name', () => {
+      var model = obj.add(testNameDevice, testWidthDevice);
+      var model2 = obj.get(testNameDevice);
+      expect(model).toEqual(model2);
+    });
+
+    it('Render devices', () => {
+      expect(obj.render()).toExist();
+    });
+
+  });
+
+  DevicesView.run();
+
+});
