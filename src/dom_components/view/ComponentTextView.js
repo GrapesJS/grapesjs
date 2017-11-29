88b992ed6fa40d3c88002f0ce122aea0ce5c60fc
Ryan Deba <ryandeba@gmail.com>
2017-11-28 18:42:12 -0600

    trigger change event instead of calling render()

--- src/dom_components/view/ComponentTextView.js
+++ src/dom_components/view/ComponentTextView.js
@@ -82,7 +82,7 @@ module.exports = ComponentView.extend({
         }
 
         // Avoid re-render on reset with silent option
-        model.set('content', '');
+        model.set('content', '').trigger('change:content', model);
         comps.add(content);
         comps.each(model => clean(model));
         comps.trigger('resetNavigator');
@@ -91,7 +91,6 @@ module.exports = ComponentView.extend({
 
     this.rteEnabled = 0;
     this.toggleEvents();
-    this.render();
   },
 
   /**
