b75091c3e196582d327bfe02b900c74bc279f743
Artur Arseniev <artur.catch@hotmail.it>
2017-12-08 12:07:45 +0100

    Refactor ComponentTableFoot

--- src/dom_components/model/ComponentTableFoot.js
+++ src/dom_components/model/ComponentTableFoot.js
@@ -1,28 +1,20 @@
-var ComponentTableBody = require('./ComponentTableBody');
+const ComponentTableBody = require('./ComponentTableBody');
 
 module.exports = ComponentTableBody.extend({
 
-  defaults: _.extend({}, ComponentTableBody.prototype.defaults, {
+  defaults: { ...ComponentTableBody.prototype.defaults,
     type: 'tfoot',
     tagName: 'tfoot',
-    droppable: ['tr']
-  }),
+  },
 
 },{
-
-  /**
-   * Detect if the passed element is a valid component.
-   * In case the element is valid an object abstracted
-   * from the element will be returned
-   * @param {HTMLElement}
-   * @return {Object}
-   * @private
-   */
   isComponent(el) {
-    var result = '';
-    if(el.tagName == 'TFOOT'){
-      result = {type: 'tfoot'};
+    let result = '';
+
+    if (el.tagName == 'TFOOT') {
+      result = { type: 'tfoot' };
     }
+
     return result;
   },
 
