6f361b896f8e85307c853f4cf0f98e1700db502e
Artur Arseniev <artur.catch@hotmail.it>
2017-12-08 12:06:34 +0100

    Refactor ComponentTableHead

--- src/dom_components/model/ComponentTableHead.js
+++ src/dom_components/model/ComponentTableHead.js
@@ -1,28 +1,20 @@
-var ComponentTableBody = require('./ComponentTableBody');
+const ComponentTableBody = require('./ComponentTableBody');
 
 module.exports = ComponentTableBody.extend({
 
-  defaults: _.extend({}, ComponentTableBody.prototype.defaults, {
+  defaults: { ...ComponentTableBody.prototype.defaults,
     type: 'thead',
-    tagName: 'thead',
-    droppable: ['tr']
-  }),
+    tagName: 'thead'
+  },
 
-},{
-
-  /**
-   * Detect if the passed element is a valid component.
-   * In case the element is valid an object abstracted
-   * from the element will be returned
-   * @param {HTMLElement}
-   * @return {Object}
-   * @private
-   */
+}, {
   isComponent(el) {
-    var result = '';
-    if(el.tagName == 'THEAD'){
-      result = {type: 'thead'};
+    let result = '';
+
+    if (el.tagName == 'THEAD') {
+      result = { type: 'thead' };
     }
+
     return result;
   },
 
