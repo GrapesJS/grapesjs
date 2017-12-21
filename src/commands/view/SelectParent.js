28f58249372806ba6ec8c77deb630714ba305fad
Artur Arseniev <artur.catch@hotmail.it>
2017-12-21 22:37:32 +0100

    Update SelectParent

--- src/commands/view/SelectParent.js
+++ src/commands/view/SelectParent.js
@@ -1,9 +1,10 @@
 module.exports = {
 
   run(editor) {
-    var comp = editor.getSelected() && editor.getSelected().parent();
+    const sel = editor.getSelected();
+    let comp = sel && sel.parent();
 
-    // recurse through the parent() chain until a selectable parent is found
+    // Recurse through the parent() chain until a selectable parent is found
     while (comp && !comp.get("selectable")) {
       comp = comp.parent();
     }
