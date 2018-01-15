ab0480d71eab969577fec209c34d9b6ef873a56c
Artur Arseniev <artur.catch@hotmail.it>
2018-01-16 00:35:07 +0100

    Use `remove` event instead of `destroy`

--- src/dom_components/model/Component.js
+++ src/dom_components/model/Component.js
@@ -176,7 +176,7 @@ const Component = Backbone.Model.extend(Styleable).extend(
       this.config = opt.config || {};
       this.ccid = Component.createId(this);
       this.set('attributes', this.get('attributes') || {});
-      this.on('destroy', this.handleRemove);
+      this.on('remove', this.handleRemove);
       this.listenTo(this, 'change:script', this.scriptUpdated);
       this.listenTo(this, 'change:traits', this.traitsUpdated);
       this.listenTo(this, 'change:tagName', this.tagUpdated);
