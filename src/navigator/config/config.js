c9a9b1c06818c8f371a7ae53ead224f063a39086
Artur Arseniev <artur.catch@hotmail.it>
2018-01-31 22:03:44 +0100

    Add el option to layers

--- src/navigator/config/config.js
+++ src/navigator/config/config.js
@@ -1,6 +1,10 @@
 module.exports = {
   stylePrefix: 'nv-',
 
+  // Specify where to render layers on init, string (query) or HTMLElement
+  // With the empty value, layers won't be visible
+  el: '',
+
   // Enable/Disable globally the possibility to sort layers
   sortable: 1,
 
