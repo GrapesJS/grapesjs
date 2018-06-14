1874fe8a2301ff65559e2c0c141f6fd0144e0ac8
Artur Arseniev <artur.catch@hotmail.it>
2018-06-14 08:25:40 +0200

    Don't copy the component only when the editor is in text editing mode

--- src/commands/view/CopyComponent.js
+++ src/commands/view/CopyComponent.js
@@ -3,7 +3,7 @@ module.exports = {
     const em = ed.getModel();
     const models = [...ed.getSelectedAll()];
 
-    if (models.length && !ed.Canvas.isInputFocused()) {
+    if (models.length && !em.isEditing()) {
       em.set('clipboard', models);
     }
   }
