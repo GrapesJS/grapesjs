7deb9bd43f37895666804b2e6285ecc7fc86a14d
Artur Arseniev <artur.catch@hotmail.it>
2018-06-17 14:22:03 +0200

    Update block defaults

--- src/block_manager/model/Block.js
+++ src/block_manager/model/Block.js
@@ -7,6 +7,8 @@ module.exports = Backbone.Model.extend({
     activate: 0,
     // If true, the dropped component will be selected
     select: 0,
+    // If true, all IDs of dropped component and its style will be changed
+    resetId: 0,
     label: '',
     content: '',
     category: '',
