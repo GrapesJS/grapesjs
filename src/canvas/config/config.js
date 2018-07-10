56b99a436562a691f722af3d056712a647759054
Patrick Spiegel <spiegel@inray.de>
2018-07-10 11:57:43 +0200

    Added onIframeLoaded option

--- src/canvas/config/config.js
+++ src/canvas/config/config.js
@@ -30,5 +30,16 @@ module.exports = {
    *  return ComponentModel.getName();
    * }
    */
-  customBadgeLabel: ''
+  customBadgeLabel: '',
+
+  /**
+   * Is called after the iframe component has been loaded
+   * @example
+   * onIframeLoad: function(FramveView) {
+   *  // do something
+   * }
+   */
+  onIframeLoad: function(iframe) {
+    console.log('iframe loaded');
+  }
 };
