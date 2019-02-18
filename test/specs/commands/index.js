60032775459bc2a9dc652dbb36290d32cfd29451
Artur Arseniev <artur.catch@hotmail.it>
2019-02-18 22:43:22 +0100

    Remove useless test

--- test/specs/commands/index.js
+++ test/specs/commands/index.js
@@ -62,29 +62,6 @@ describe('Commands', () => {
       expect(obj.get('test').test).toEqual('test');
     });
 
-    test('Load default commands at init', () => {
-      expect(obj.get('select-comp')).not.toBeUndefined();
-      expect(obj.get('create-comp')).not.toBeUndefined();
-      expect(obj.get('delete-comp')).not.toBeUndefined();
-      expect(obj.get('move-comp')).not.toBeUndefined();
-      expect(obj.get('export-template')).not.toBeUndefined();
-      expect(obj.get('sw-visibility')).not.toBeUndefined();
-      expect(obj.get('open-layers')).not.toBeUndefined();
-      expect(obj.get('open-sm')).not.toBeUndefined();
-      expect(obj.get('open-tm')).not.toBeUndefined();
-      expect(obj.get('open-blocks')).not.toBeUndefined();
-      expect(obj.get('open-assets')).not.toBeUndefined();
-      expect(obj.get('show-offset')).not.toBeUndefined();
-      expect(obj.get('select-parent')).not.toBeUndefined();
-      expect(obj.get('tlb-delete')).not.toBeUndefined();
-      expect(obj.get('tlb-clone')).not.toBeUndefined();
-      expect(obj.get('tlb-move')).not.toBeUndefined();
-      expect(obj.get('fullscreen')).not.toBeUndefined();
-      expect(obj.get('preview')).not.toBeUndefined();
-      expect(obj.get('resize')).not.toBeUndefined();
-      expect(obj.get('drag')).not.toBeUndefined();
-    });
-
     test('Default commands after loadDefaultCommands', () => {
       obj.loadDefaultCommands();
       expect(obj.get('select-comp')).not.toBeUndefined();
