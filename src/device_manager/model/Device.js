6aaf0a9adfaaec4bd1c18aaf3f54c973bac5ddf3
Artur Arseniev <artur.catch@hotmail.it>
2019-03-03 14:07:54 +0100

    Ensure default units for devices

--- src/device_manager/model/Device.js
+++ src/device_manager/model/Device.js
@@ -8,7 +8,7 @@ module.exports = Backbone.Model.extend({
     name: '',
 
     // Width to set for the editor iframe
-    width: '',
+    width: null,
 
     // Height to set for the editor iframe
     height: '',
@@ -24,7 +24,16 @@ module.exports = Backbone.Model.extend({
   initialize() {
     this.get('widthMedia') === null &&
       this.set('widthMedia', this.get('width'));
+    this.get('width') === null && this.set('width', this.get('widthMedia'));
     !this.get('priority') &&
       this.set('priority', parseFloat(this.get('widthMedia')) || 0);
+    const toCheck = ['width', 'height', 'widthMedia'];
+    toCheck.forEach(prop => this.checkUnit(prop));
+  },
+
+  checkUnit(prop) {
+    const pr = this.get(prop);
+    const noUnit = (parseFloat(pr) || 0).toString() === pr.toString();
+    noUnit && this.set(prop, `${pr}px`);
   }
 });
