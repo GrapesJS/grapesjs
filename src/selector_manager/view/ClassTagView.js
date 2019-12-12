8d1aaa8e503cb85829347ba773ac68a0fc37bf96
Artur Arseniev <artur.catch@hotmail.it>
2019-12-12 09:01:30 +0100

    Update tags layout

--- src/selector_manager/view/ClassTagView.js
+++ src/selector_manager/view/ClassTagView.js
@@ -8,10 +8,10 @@ export default Backbone.View.extend({
     const label = model.get('label') || '';
 
     return `
-      <span id="${pfx}checkbox" class="fa" data-tag-status></span>
+      <span id="${pfx}checkbox" class="${pfx}tag-status" data-tag-status></span>
       <span id="${pfx}tag-label" data-tag-name>${label}</span>
-      <span id="${pfx}close" data-tag-remove>
-        &Cross;
+      <span id="${pfx}close" class="${pfx}tag-close" data-tag-remove>
+        <svg viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"></path></svg>
       </span>
     `;
   },
@@ -108,15 +108,17 @@ export default Backbone.View.extend({
    */
   updateStatus() {
     const { model, $el } = this;
-    const chkOn = 'fa-check-square-o';
-    const chkOff = 'fa-square-o';
     const $chk = $el.find('[data-tag-status]');
+    const iconOff =
+      '<svg viewBox="0 0 24 24"><path d="M19 3H5c-1.11 0-2 .89-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5a2 2 0 0 0-2-2m0 2v14H5V5h14z"></path></svg>';
+    const iconOn =
+      '<svg viewBox="0 0 24 24"><path d="M19 19H5V5h10V3H5c-1.11 0-2 .89-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-8h-2m-11.09-.92L6.5 11.5 11 16 21 6l-1.41-1.42L11 13.17l-3.09-3.09z"></path></svg>';
 
     if (model.get('active')) {
-      $chk.removeClass(chkOff).addClass(chkOn);
+      $chk.html(iconOn);
       $el.removeClass('opac50');
     } else {
-      $chk.removeClass(chkOn).addClass(chkOff);
+      $chk.html(iconOff);
       $el.addClass('opac50');
     }
   },
