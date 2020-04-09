9cc99f4a24885d3dbf02429161001781be7296b8
Artur Arseniev <artur.catch@hotmail.it>
2020-04-09 02:07:21 +0200

    Fix PropertyColorView tests

--- src/domain_abstract/ui/InputColor.js
+++ src/domain_abstract/ui/InputColor.js
@@ -56,16 +56,16 @@ export default Input.extend({
    */
   getColorEl() {
     if (!this.colorEl) {
+      const { em } = this;
       const self = this;
       const ppfx = this.ppfx;
       var model = this.model;
 
       var colorEl = $(`<div class="${this.ppfx}field-color-picker"></div>`);
       var cpStyle = colorEl.get(0).style;
-      var elToAppend = this.em && this.em.config ? this.em.config.el : '';
+      var elToAppend = em && em.config ? em.config.el : '';
       var colorPickerConfig =
-        (this.em && this.em.getConfig && this.em.getConfig('colorPicker')) ||
-        {};
+        (em && em.getConfig && em.getConfig('colorPicker')) || {};
       const getColor = color => {
         let cl =
           color.getAlpha() == 1 ? color.toHexString() : color.toRgbString();
@@ -117,8 +117,9 @@ export default Input.extend({
         }
       });
 
-      this.em &&
-        this.em.on('component:selected', () => {
+      em &&
+        em.on &&
+        em.on('component:selected', () => {
           changed = 1;
           colorEl.spectrum('hide');
         });
