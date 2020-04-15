9a3d0e11b58224ca2d80673289d1a690c9009eec
Artur Arseniev <artur.catch@hotmail.it>
2020-04-16 01:47:34 +0200

    Fix TableRow model component. Closes #2697

--- src/dom_components/model/ComponentTableRow.js
+++ src/dom_components/model/ComponentTableRow.js
@@ -4,31 +4,12 @@ export default Component.extend(
   {
     defaults: {
       ...Component.prototype.defaults,
-      type: 'row',
       tagName: 'tr',
       draggable: ['thead', 'tbody', 'tfoot'],
       droppable: ['th', 'td']
-    },
-
-    initialize(o, opt) {
-      Component.prototype.initialize.apply(this, arguments);
-
-      // Clean the row from non cell components
-      const cells = [];
-      const components = this.get('components');
-      components.each(model => model.is('cell') && cells.push(model));
-      components.reset(cells);
     }
   },
   {
-    isComponent(el) {
-      let result = '';
-
-      if (el.tagName == 'TR') {
-        result = { type: 'row' };
-      }
-
-      return result;
-    }
+    isComponent: el => el.tagName == 'TR' && true
   }
 );
