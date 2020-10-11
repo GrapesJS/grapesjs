b66ba656f9429fef3102aa882fe30331c125e3cf
Artur Arseniev <artur.catch@hotmail.it>
2020-10-11 20:03:00 +0200

    Add `parse:css` event

--- src/parser/model/ParserCss.js
+++ src/parser/model/ParserCss.js
@@ -16,6 +16,7 @@ export default (config = {}) => ({
     const editor = em && em.get && em.get('Editor');
     const nodes = parserCss ? parserCss(str, editor) : BrowserCssParser(str);
     nodes.forEach(node => (result = result.concat(this.checkNode(node))));
+    em && em.trigger('parse:css', { input: str, output: result });
 
     return result;
   },
