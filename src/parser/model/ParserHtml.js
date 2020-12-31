c2c64cc45cde20174acd31a23f50057e6615bb13
Artur Arseniev <artur.catch@hotmail.it>
2020-12-31 14:02:11 +0100

    Create text nodes during HTML parsing

--- src/parser/model/ParserHtml.js
+++ src/parser/model/ParserHtml.js
@@ -202,7 +202,10 @@ export default config => {
           // just make it content of the current node
           if (nodeChild === 1 && firstChild.nodeType === 3) {
             !model.type && (model.type = 'text');
-            model.components = firstChild.nodeValue;
+            model.components = {
+              type: 'textnode',
+              content: firstChild.nodeValue
+            };
           } else {
             model.components = this.parseNode(node);
           }
