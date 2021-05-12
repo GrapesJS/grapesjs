721024ff1fb8c719a6fe4989fd6388532756f9eb
artenc <arten.c.g@gmail.com>
2021-05-12 17:12:34 +0300

    Generated HTML should not contain editonly classes

--- src/dom_components/model/Component.js
+++ src/dom_components/model/Component.js
@@ -565,9 +565,13 @@ const Component = Backbone.Model.extend(Styleable).extend(
 
       // Add classes
       if (!opts.noClass) {
-        this.get('classes').forEach(cls =>
-          classes.push(isString(cls) ? cls : cls.get('name'))
-        );
+        this.get('classes').forEach(cls => {
+          if (isString(cls)) {
+            classes.push(cls);
+          } else if (!opts.forHTML || !cls.get('editonly')) {
+            classes.push(cls.get('name'));
+          }
+        });
         classes.length && (attributes.class = classes.join(' '));
       }
 
@@ -1382,7 +1386,7 @@ const Component = Backbone.Model.extend(Styleable).extend(
      * @private
      */
     getAttrToHTML() {
-      var attr = this.getAttributes();
+      var attr = this.getAttributes({ forHTML: true });
       delete attr.style;
       return attr;
     },
