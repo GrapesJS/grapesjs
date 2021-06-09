be562eea67082709045e72b4dc3161ec677d8b45
Artur Arseniev <artur.catch@hotmail.it>
2021-06-09 20:38:58 +0200

    Add Pages class

--- src/pages/model/Pages.js
+++ src/pages/model/Pages.js
@@ -1,26 +1,26 @@
 import { Collection } from 'backbone';
 import Page from './Page';
 
-export default Collection.extend({
-  model: Page,
-
+export default class Pages extends Collection {
   initialize(models, config = {}) {
     this.config = config;
     this.on('reset', this.onReset);
     this.on('remove', this.onRemove);
-  },
+  }
 
   onReset(m, opts = {}) {
     const prev = opts.previousModels || [];
     prev.map(p => this.onRemove(p));
-  },
+  }
 
   onRemove(removed) {
     removed && removed.onRemove();
-  },
+  }
 
   add(m, o = {}) {
     const { config } = this;
     return Collection.prototype.add.call(this, m, { ...o, config });
   }
-});
+}
+
+Pages.prototype.model = Page;
