6ceff6a32c3660e2cab7c92563f1149e8b8cb7e3
Artur Arseniev <artur.catch@hotmail.it>
2021-08-04 08:49:05 +0200

    Make CommandAbstract a model

--- test/specs/grapesjs/headless.js
+++ test/specs/grapesjs/headless.js
@@ -6,5 +6,6 @@ describe('GrapesJS Headless', () => {
   test('Can init editor', () => {
     const editor = grapesjs.init({ headless: true });
     expect(editor).toBeTruthy();
+    editor.destroy();
   });
 });
