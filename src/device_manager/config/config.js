4bff50934b49968852a2af75faaf3e908943b52b
Artur Arseniev <artur.catch@hotmail.it>
2021-08-12 13:27:24 +0200

    Move devices defaults

--- src/device_manager/config/config.js
+++ src/device_manager/config/config.js
@@ -1,3 +1,27 @@
 export default {
-  devices: []
+  devices: [
+    {
+      id: 'desktop',
+      name: 'Desktop',
+      width: ''
+    },
+    {
+      id: 'tablet',
+      name: 'Tablet',
+      width: '768px',
+      widthMedia: '992px'
+    },
+    {
+      id: 'mobileLandscape',
+      name: 'Mobile landscape',
+      width: '568px',
+      widthMedia: '768px'
+    },
+    {
+      id: 'mobilePortrait',
+      name: 'Mobile portrait',
+      width: '320px',
+      widthMedia: '480px'
+    }
+  ]
 };
--- src/editor/config/config.js
+++ src/editor/config/config.js
@@ -212,33 +212,7 @@ export default {
   selectorManager: {},
 
   //Configurations for Device Manager
-  deviceManager: {
-    devices: [
-      {
-        id: 'desktop',
-        name: 'Desktop',
-        width: ''
-      },
-      {
-        id: 'tablet',
-        name: 'Tablet',
-        width: '768px',
-        widthMedia: '992px'
-      },
-      {
-        id: 'mobileLandscape',
-        name: 'Mobile landscape',
-        width: '568px',
-        widthMedia: '768px'
-      },
-      {
-        id: 'mobilePortrait',
-        name: 'Mobile portrait',
-        width: '320px',
-        widthMedia: '480px'
-      }
-    ]
-  },
+  deviceManager: {},
 
   //Configurations for Style Manager
   styleManager: {
