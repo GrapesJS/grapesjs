cf5be4c1989d583feb694843e59fd06045fe3acb
Artur Arseniev <artur.catch@hotmail.it>
2021-08-14 15:30:58 +0200

    Add onClick property to Block

--- src/block_manager/model/Block.js
+++ src/block_manager/model/Block.js
@@ -9,6 +9,7 @@ import { Model } from 'common';
  * @property {Boolean} [select=false] If true, the dropped component will be selected
  * @property {Boolean} [resetId=false] If true, all IDs of dropped components and their styles will be changed
  * @property {Boolean} [disable=false] Disable the block from being interacted
+ * @property {Function} [onClick] Custom behavior on click, eg. `(editor, block) => editor.getWrapper().append(block.get('content'))`
  * @property {Object} [attributes={}] Block attributes to apply in the view element
  */
 export default class Block extends Model {
@@ -22,6 +23,7 @@ export default class Block extends Model {
       select: false,
       resetId: false,
       disable: false,
+      onClick: null,
       attributes: {}
     };
   }
