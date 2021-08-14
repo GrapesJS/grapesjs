bd24c7af4dd4e598e8fa67004cc8167893c743c4
Artur Arseniev <artur.catch@hotmail.it>
2021-08-14 14:49:38 +0200

    Update block

--- src/block_manager/model/Block.js
+++ src/block_manager/model/Block.js
@@ -8,7 +8,7 @@ import { Model } from 'common';
  * @property {Boolean} [activate=false] If true, triggers an the `active` event on dropped component
  * @property {Boolean} [select=false] If true, the dropped component will be selected
  * @property {Boolean} [resetId=false] If true, all IDs of dropped components and their styles will be changed
- * @property {Boolean} [disable=false] Disable the drag of the block
+ * @property {Boolean} [disable=false] Disable the block from being interacted
  * @property {Object} [attributes={}] Block attributes to apply in the view element
  */
 export default class Block extends Model {
