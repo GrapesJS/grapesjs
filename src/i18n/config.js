4a912d4400696dfcdcf749c4eecc04a5ec1c0da9
Artur Arseniev <artur.catch@hotmail.it>
2021-12-27 12:02:10 +0100

    Update i18n config

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -8,13 +8,16 @@ export default {
   localeFallback: 'en',
 
   // Detect locale by checking browser language
-  detectLocale: 1,
+  detectLocale: true,
 
   // Show warnings when some of the i18n resources are missing
-  debug: 0,
+  debug: false,
 
   // Messages to translate
   messages: {
-    en
-  }
+    en,
+  },
+
+  // Additional messages. This allows extending the default `messages` set directly from the configuration.
+  messagesAdd: null,
 };
