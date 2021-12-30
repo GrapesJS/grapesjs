c2502f82365584ace421fb49421b8b1151e7303d
Artur Arseniev <artur.catch@hotmail.it>
2021-12-30 13:16:16 +0100

    Move storage manager events

--- src/editor/index.js
+++ src/editor/index.js
@@ -45,18 +45,8 @@
  * * `styleManager:update:target` - The target (Component or CSSRule) is changed
  * * `styleManager:change` - Triggered on style property change from new selected component, the view of the property is passed as an argument to the callback
  * * `styleManager:change:{propertyName}` - As above but for a specific style property
- * ### Storages
- * * `storage:start` - Before the storage request is started
- * * `storage:start:store` - Before the store request. The object to store is passed as an argumnet (which you can edit)
- * * `storage:start:load` - Before the load request. Items to load are passed as an argumnet (which you can edit)
- * * `storage:load` - Triggered when something was loaded from the storage, loaded object passed as an argumnet
- * * `storage:store` - Triggered when something is stored to the storage, stored object passed as an argumnet
- * * `storage:end` - After the storage request is ended
- * * `storage:end:store` - After the store request
- * * `storage:end:load` - After the load request
- * * `storage:error` - On any error on storage request, passes the error as an argument
- * * `storage:error:store` - Error on store request, passes the error as an argument
- * * `storage:error:load` - Error on load request, passes the error as an argument
+ * ### Storage
+ * Check the [Storage](/api/storage_manager.html) module.
  * ### Canvas
  * Check the [Canvas](/api/canvas.html) module.
  * ### RTE
