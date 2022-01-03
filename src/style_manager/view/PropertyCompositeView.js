3139befbf90d0fc74ea8c97cc36bd2ed042dd911
Artur Arseniev <artur.catch@hotmail.it>
2022-01-03 17:37:47 +0100

    Ignore input changes in composite view

--- src/style_manager/view/PropertyCompositeView.js
+++ src/style_manager/view/PropertyCompositeView.js
@@ -11,17 +11,12 @@ export default PropertyView.extend({
     `;
   },
 
-  inputValueChanged(...args) {
-    // If it's not detached (eg. 'padding: 1px 2px 3px 4px;') it will follow the same flow of the PropertyView
-    if (!this.model.isDetached()) {
-      PropertyView.prototype.inputValueChanged.apply(this, args);
-    }
-  },
-
   remove() {
     this.props?.remove();
     PropertyView.prototype.remove.apply(this, arguments);
   },
+
+  onValueChange() {},
 
   onRender() {
     const { model, pfx } = this;
