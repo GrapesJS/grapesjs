551e0ec32a4340525725662d662cb4e5a67482f7
Artur Arseniev <artur.catch@hotmail.it>
2022-04-23 07:42:22 +0200

    Fix index test

--- test/specs/grapesjs/index.js
+++ test/specs/grapesjs/index.js
@@ -99,10 +99,10 @@ describe('GrapesJS', () => {
     test('Editor canvas baseCSS defaults to sensible values if not defined', () => {
       config.components = htmlString;
       config.protectedCss = '';
-
-      var editor = obj.init(config);
-
-      expect(window.frames[0].document.documentElement.outerHTML.replace(/\s+/g, ` `)).toContain(`body { margin: 0;`);
+      obj.init(config);
+      expect(window.frames[0].document.documentElement.outerHTML.replace(/\s+/g, ` `)).toContain(
+        `body { background-color: #fff`
+      );
     });
 
     test('Init editor with html', () => {
