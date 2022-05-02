d9dc509a5a538da5385f373a5f47981fd90876bf
Artur Arseniev <artur.catch@hotmail.it>
2022-05-02 12:06:16 +0200

    Update root update in LayerManager. Closes #4083

--- src/navigator/index.ts
+++ src/navigator/index.ts
@@ -1,7 +1,8 @@
+import { isString } from 'underscore';
 import { Model } from '../abstract';
 import Module from '../abstract/Module';
 import Component from '../dom_components/model/Component';
-import { hasWin } from '../utils/mixins';
+import { hasWin, isComponent } from '../utils/mixins';
 import defaults from './config/config';
 import View from './view/ItemView';
 
@@ -17,22 +18,18 @@ export default class LayerManager extends Module<typeof defaults> {
     init() {
       this.__initDefaults(defaults);
       this.componentChanged = this.componentChanged.bind(this);
-      const model = new Model(this);
-      this.model = model;
-      // model.on(chnSel, this._onRootChange);
+      this.__onRootChange = this.__onRootChange.bind(this);
+      this.model = new Model(this);
       // @ts-ignore
       this.config.stylePrefix = this.config.pStylePrefix;
       return this;
     }
 
-    postLoad() {
-      const { root } = this.config;
-      const rootCmp = root && hasWin() ? root : this.em.getWrapper();
-      this.setRoot(rootCmp);
-    }
-
     onLoad() {
-      this.model?.listenTo(this.em, 'component:selected', this.componentChanged);
+      const { em, config, model } = this;
+      model?.listenTo(em, 'component:selected', this.componentChanged);
+      model?.listenToOnce(em, 'load', () => this.setRoot(config.root));
+      model?.on('change:root', this.__onRootChange);
       this.componentChanged();
     }
 
@@ -42,12 +39,20 @@ export default class LayerManager extends Module<typeof defaults> {
 
     /**
      * Set new root for layers
-     * @param {HTMLElement|Component|String} el Component to be set as the root
-     * @return {self}
+     * @param {Component|string} component Component to be set as the root
+     * @return {Component}
      */
-    setRoot(el: HTMLElement | string) {
-      this.view?.setRoot(el);
-      return this;
+    setRoot(component: Component | string): Component {
+      const wrapper: Component = this.em.getWrapper();
+      let root = isComponent(component) ? component as Component : wrapper;
+
+      if (component && isString(component) && hasWin()) {
+        root = wrapper.find(component)[0] || wrapper;
+      }
+
+      this.model?.set('root', root);
+
+      return root;
     }
 
     /**
@@ -55,7 +60,7 @@ export default class LayerManager extends Module<typeof defaults> {
      * @return {Component}
      */
     getRoot() {
-      return this.view?.model;
+      return this.model?.get('root');
     }
 
     /**
@@ -70,43 +75,50 @@ export default class LayerManager extends Module<typeof defaults> {
      * Triggered when the selected component is changed
      * @private
      */
-    componentChanged(selected?: Component, opts = {}) {
+    componentChanged(sel?: Component, opts = {}) {
       // @ts-ignore
       if (opts.fromLayers) return;
       const { em, config } = this;
+      const { scrollLayers } = config;
       const opened = em.get('opened');
-      const model = em.getSelected();
-      const scroll = config.scrollLayers;
-      let parent = model && model.collection ? model.collection.parent : null;
-      for (let cid in opened) opened[cid].set('open', 0);
+      const selected = em.getSelected();
+      let parent = selected?.parent();
+
+      for (let cid in opened) {
+        opened[cid].set('open', 0);
+      }
 
       while (parent) {
         parent.set('open', 1);
         opened[parent.cid] = parent;
-        parent = parent.collection ? parent.collection.parent : null;
+        parent = parent.parent();
       }
 
-      if (model && scroll) {
-        const el = model.viewLayer && model.viewLayer.el;
-        el && el.scrollIntoView(scroll);
+      if (selected && scrollLayers) {
+        const el = selected.viewLayer?.el;
+        el?.scrollIntoView(scrollLayers);
       }
     }
 
     render() {
       const { em, config } = this;
       const ItemView = View.extend(config.extend);
-      this.view?.remove();
       this.view = new ItemView({
+        el: this.view?.el,
         ItemView,
         level: 0,
         config,
         opened: em.get('opened') || {},
-        model: em.getWrapper(),
+        model: this.getRoot(),
       });
       return this.view?.render().el as HTMLElement;
     }
 
     destroy() {
       this.view?.remove();
+    }
+
+    __onRootChange() {
+      this.view?.setRoot(this.getRoot());
     }
 };
