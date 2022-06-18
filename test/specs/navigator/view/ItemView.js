ac22ab6a94316a6f7f06e1ed50aef1bef0f8bf03
Artur Arseniev <artur.catch@hotmail.it>
2022-06-18 12:52:39 +0200

    Fix tests

--- test/specs/navigator/view/ItemView.js
+++ test/specs/navigator/view/ItemView.js
@@ -3,27 +3,19 @@ import config from 'navigator/config/config';
 import EditorModel from 'editor/model/Editor';
 
 describe('ItemView', () => {
-  let itemView, fakeModel, fakeModelStyle;
+  let itemView;
 
   const isVisible = itemView => {
     return itemView.module.isVisible(itemView.model);
   };
 
   beforeEach(() => {
-    fakeModelStyle = {};
-
-    fakeModel = {
-      get: jest.fn(),
-      set: jest.fn(),
-      getStyle: jest.fn(() => fakeModelStyle),
-    };
-
     const em = new EditorModel();
-    const module = em.get('LayerManager');
+    const defCmp = em.get('DomComponents').getType('default').model;
 
     itemView = new ItemView({
-      model: fakeModel,
-      module,
+      model: new defCmp({}, { em }),
+      module: em.get('LayerManager'),
       config: { ...config, em },
     });
   });
@@ -31,11 +23,11 @@ describe('ItemView', () => {
   describe('.isVisible', () => {
     it("should return `false` if the model's `style` object has a `display` property set to `none`, `true` otherwise", () => {
       expect(isVisible(itemView)).toEqual(true);
-      fakeModelStyle.display = '';
+      itemView.model.addStyle({ display: '' });
       expect(isVisible(itemView)).toEqual(true);
-      fakeModelStyle.display = 'none';
+      itemView.model.addStyle({ display: 'none' });
       expect(isVisible(itemView)).toEqual(false);
-      fakeModelStyle.display = 'block';
+      itemView.model.addStyle({ display: 'block' });
       expect(isVisible(itemView)).toEqual(true);
     });
   });
