2cf59db26cc044efb22eb0ab5f8c969a77913be1
Alex <ritt.alex@gmail.com>
2022-07-09 20:53:40 +0200

    Update ModalView test

--- test/specs/modal/view/ModalView.js
+++ test/specs/modal/view/ModalView.js
@@ -1,13 +1,15 @@
 import ModalView from 'modal_dialog/view/ModalView';
 import Modal from 'modal_dialog/model/Modal';
+import Editor from 'editor';
 
 describe('ModalView', () => {
   var model;
   var view;
-  var editorModel;
+  var em;
 
   beforeEach(() => {
-    model = new Modal();
+    em = new Editor({});
+    model = new Modal(em);
     view = new ModalView({
       model,
     });
