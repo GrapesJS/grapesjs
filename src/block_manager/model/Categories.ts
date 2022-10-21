3d6f4f6f082c337614be8575a2fddf1cec0f7799
Artur Arseniev <artur.catch@hotmail.it>
2022-10-21 15:17:17 +0400

    Move Categories to TS

--- src/block_manager/model/Categories.ts
+++ src/block_manager/model/Categories.ts
@@ -1,6 +1,6 @@
 import { Collection } from '../../common';
 import Category from './Category';
 
-export default class Categories extends Collection {}
+export default class Categories extends Collection<Category> {}
 
 Categories.prototype.model = Category;
