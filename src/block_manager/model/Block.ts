7c70be6380ef43eb820e6ad251e15dd8e82eaa93
Artur Arseniev <artur.catch@hotmail.it>
2022-10-21 15:03:16 +0400

    Move Block to TS

--- src/block_manager/model/Block.ts
+++ src/block_manager/model/Block.ts
@@ -1,6 +1,77 @@
 import { Model } from '../../common';
 import { isFunction } from 'underscore';
 
+// TODO move
+interface BlockCategoryProperties {
+  /**
+   * Category id.
+   */
+  id: string;
+  /**
+   * Category label.
+   */
+  label: string;
+  /**
+   * Category open state.
+   */
+  open?: boolean;
+  /**
+   * Category attributes.
+   */
+  attributes?: Record<string, any>;
+}
+
+interface BlockProperties {
+  /**
+   * Block label, eg. `My block`
+   */
+  label: string;
+  /**
+   * The content of the block. Might be an HTML string or a [Component Defintion](/modules/Components.html#component-definition)
+   */
+  content: string | any;
+  /**
+   * HTML string for the media/icon of the block, eg. `<svg ...`, `<img ...`, etc.
+   * @default ''
+   */
+  media?: string;
+  /**
+   * Block category, eg. `Basic blocks`
+   * @default ''
+   */
+  category?: string | BlockCategoryProperties;
+  /**
+   * If true, triggers the `active` event on the dropped component.
+   * @default false
+   */
+  activate?: boolean;
+  /**
+   * If true, the dropped component will be selected.
+   * @default false
+   */
+  select?: boolean;
+  /**
+   * If true, all IDs of dropped components and their styles will be changed.
+   * @default false
+   */
+  resetId?: boolean;
+  /**
+   * Disable the block from being interacted.
+   * @default false
+   */
+  disable?: boolean;
+  /**
+   * Custom behavior on click.
+   * @example
+   * onClick: (block, editor) => editor.getWrapper().append(block.get('content'))
+   */
+  onClick?: (...params: any[]) => any; // TODO
+  /**
+   * Block attributes
+   */
+  attributes?: Record<string, any>;
+}
+
 /**
  * @property {String} label Block label, eg. `My block`
  * @property {String|Object} content The content of the block. Might be an HTML string or a [Component Defintion](/modules/Components.html#component-definition)
@@ -15,7 +86,7 @@ import { isFunction } from 'underscore';
  *
  * @module docsjs.Block
  */
-export default class Block extends Model {
+export default class Block extends Model<BlockProperties> {
   defaults() {
     return {
       label: '',
@@ -23,10 +94,10 @@ export default class Block extends Model {
       media: '',
       category: '',
       activate: false,
-      select: null,
+      select: undefined,
       resetId: false,
       disable: false,
-      onClick: null,
+      onClick: undefined,
       attributes: {},
     };
   }
@@ -36,7 +107,7 @@ export default class Block extends Model {
    * @returns {String}
    */
   getId() {
-    return this.id;
+    return this.id as string;
   }
 
   /**
@@ -44,7 +115,7 @@ export default class Block extends Model {
    * @returns {String}
    */
   getLabel() {
-    return this.get('label');
+    return this.get('label')!;
   }
 
   /**
@@ -67,8 +138,9 @@ export default class Block extends Model {
    * Get block category label
    * @returns {String}
    */
-  getCategoryLabel() {
+  getCategoryLabel(): string {
     const ctg = this.get('category');
-    return isFunction(ctg.get) ? ctg.get('label') : ctg.label ? ctg.label : ctg;
+    // @ts-ignore
+    return isFunction(ctg?.get) ? ctg.get('label') : ctg?.label ? ctg?.label : ctg;
   }
 }
