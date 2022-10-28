891a5e31b13657df0f22d0a8dd92d6d2bd351057
Artur Arseniev <artur.catch@hotmail.it>
2022-10-28 14:06:35 +0400

    Up

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,7 +7,7 @@ import EditorModel from '../editor/model/Editor';
 
 export default class UtilsModule extends Module {
   Sorter = Sorter;
-  Resizer: typeof Resizer = Resizer;
+  Resizer = Resizer;
   Dragger = Dragger;
   helpers = { ...mixins };
 
