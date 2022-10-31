81d6ee912b122037ddd920dbec1186dd9dce18be
Artur Arseniev <artur.catch@hotmail.it>
2022-10-31 15:20:56 +0400

    Move parser config to TS

---
+++ src/parser/config/config.ts
@@ -0,0 +1,64 @@
+interface HTMLParserOptions {
+  /**
+   * DOMParser mime type.
+   * If you use the `text/html` parser, it will fix the invalid syntax automatically.
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/DOMParser/parseFromString
+   * @default 'text/html'
+   */
+  htmlType?: string;
+
+  /**
+   * Allow <script> tags.
+   * @default false
+   */
+  allowScripts?: boolean;
+
+  /**
+   * Allow unsafe HTML attributes (eg. `on*` inline event handlers).
+   * @default false
+   */
+  allowUnsafeAttr?: boolean;
+}
+
+export interface ParserConfig {
+  /**
+   * Let the editor know which HTML tags should be treated as part of the text component.
+   * @default ['br', 'b', 'i', 'u', 'a', 'ul', 'ol']
+   */
+  textTags?: string[];
+
+  /**
+   * Custom CSS parser.
+   * @see https://grapesjs.com/docs/guides/Custom-CSS-parser.html
+   */
+  parserCss?: any; // TODO
+
+  /**
+   * Custom HTML parser.
+   * At the moment, the custom HTML parser should rely on DOM Node instance as the result.
+   * @example
+   * // The return should be an instance of an Node as the root to traverse
+   * // https://developer.mozilla.org/en-US/docs/Web/API/Node
+   * // Here the result will be XMLDocument, which extends Node.
+   * parserHtml: (input, opts = {}) => (new DOMParser()).parseFromString(input, 'text/xml')
+   */
+  parserHtml?: any; // TODO
+
+  /**
+   * Default HTML parser options (used in `parserModule.parseHtml('<div...', options)`).
+   */
+  optionsHtml?: HTMLParserOptions;
+}
+
+const config: ParserConfig = {
+  textTags: ['br', 'b', 'i', 'u', 'a', 'ul', 'ol'],
+  parserCss: null,
+  parserHtml: null,
+  optionsHtml: {
+    htmlType: 'text/html',
+    allowScripts: false,
+    allowUnsafeAttr: false,
+  },
+};
+
+export default config;
