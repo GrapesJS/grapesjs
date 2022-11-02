9ff4a1766297bf7494d1f6259e913a27d5b3aa43
Artur Arseniev <artur.catch@hotmail.it>
2022-11-02 09:22:33 +0400

    Up tests

--- test/specs/code_manager/index.js
+++ test/specs/code_manager/index.js
@@ -1,11 +1,13 @@
 import CodeManager from 'code_manager';
+import Editor from 'editor/model/Editor';
 
 describe('Code Manager', () => {
   describe('Main', () => {
     let obj;
 
     beforeEach(() => {
-      obj = new CodeManager();
+      const em = new Editor({});
+      obj = new CodeManager(em);
     });
 
     afterEach(() => {
@@ -14,14 +16,6 @@ describe('Code Manager', () => {
 
     test('Object exists', () => {
       expect(CodeManager).toBeTruthy();
-    });
-
-    test('No code generators inside', () => {
-      expect(obj.getGenerators()).toEqual({});
-    });
-
-    test('No code viewers inside', () => {
-      expect(obj.getViewers()).toEqual({});
     });
 
     test('Add and get code generator', () => {
