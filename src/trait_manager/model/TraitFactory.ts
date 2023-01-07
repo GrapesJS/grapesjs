43f658f789e1ecb96fba08cbb224622b68c2dbd3
Artur Arseniev <artur.catch@hotmail.it>
2023-01-07 14:23:37 +0400

    Move TraitFactory to TS

--- src/trait_manager/model/TraitFactory.ts
+++ src/trait_manager/model/TraitFactory.ts
@@ -1,18 +1,23 @@
-export default (config = {}) => ({
+import { TraitManagerConfig } from '../config/config';
+import { TraitProperties } from './Trait';
+
+export default (config: Partial<TraitManagerConfig> = {}) => ({
   /**
    * Build props object by their name
    * @param  {Array<string>|string} props Array of properties name
    * @return {Array<Object>}
    */
-  build(props) {
+  build(props: string | string[]) {
     const objs = [];
 
     if (typeof props === 'string') props = [props];
 
     for (let i = 0; i < props.length; i++) {
-      const obj = {};
       const prop = props[i];
-      obj.name = prop;
+      const obj: TraitProperties = {
+        name: prop,
+        type: 'text',
+      };
 
       switch (prop) {
         case 'target':
