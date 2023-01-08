0f653134e8823241a81d23891847e3a594ada593
Artur Arseniev <artur.catch@hotmail.it>
2023-01-08 11:06:42 +0400

    Move Commands to TS

--- src/commands/model/Commands.ts
+++ src/commands/model/Commands.ts
@@ -1,6 +1,6 @@
 import { Collection } from '../../common';
 import Command from './Command';
 
-export default class Commands extends Collection {}
+export default class Commands extends Collection<Command> {}
 
 Commands.prototype.model = Command;
