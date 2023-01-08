2ce1bd848fe912cc4fb657ea2e4b84bd0c7e5b12
Artur Arseniev <artur.catch@hotmail.it>
2023-01-08 12:12:38 +0400

    TS CopyComponent

--- src/commands/view/CopyComponent.ts
+++ src/commands/view/CopyComponent.ts
@@ -1,7 +1,9 @@
+import { CustomCommand } from './CommandAbstract';
+
 export default {
   run(ed) {
     const em = ed.getModel();
     const models = [...ed.getSelectedAll()];
     models.length && em.set('clipboard', models);
   },
-};
+} as CustomCommand;
