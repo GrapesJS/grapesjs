e31facee9d0fbe412bd6f146e1418dbb1e0d2771
Artur Arseniev <artur.catch@hotmail.it>
2023-01-09 12:06:09 +0400

    Move style Layers to TS

--- src/style_manager/model/Layers.ts
+++ src/style_manager/model/Layers.ts
@@ -1,8 +1,10 @@
 import { Collection } from '../../common';
 import Layer from './Layer';
 
-export default class Layers extends Collection {
-  initialize(p, opts = {}) {
+export default class Layers extends Collection<Layer> {
+  prop: any;
+
+  initialize(p: any, opts: { prop?: any } = {}) {
     this.prop = opts.prop;
   }
 }
