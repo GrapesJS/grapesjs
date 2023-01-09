a713ec7d9b32681e9e22ae4a850cfb4c2aea061c
Artur Arseniev <artur.catch@hotmail.it>
2023-01-09 11:13:46 +0400

    Remove unused files

--- src/commands/model/Command.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from '../../common';
-
-export default class Command extends Model {
-  defaults() {
-    return {
-      id: '',
-    };
-  }
-}
--- src/commands/model/Commands.ts
+++
@@ -1,6 +0,0 @@
-import { Collection } from '../../common';
-import Command from './Command';
-
-export default class Commands extends Collection<Command> {}
-
-Commands.prototype.model = Command;
