e642a51326c50d8bb4d0bbb86276a225ec46dcdb
Artur Arseniev <artur.catch@hotmail.it>
2023-01-19 17:32:24 +0400

    Move ComponentTableRow to TS

--- src/dom_components/model/ComponentTableRow.ts
+++ src/dom_components/model/ComponentTableRow.ts
@@ -1,17 +1,20 @@
 import Component from './Component';
-import { toLowerCase } from 'utils/mixins';
+import { toLowerCase } from '../../utils/mixins';
 
 const tagName = 'tr';
 
 export default class ComponentTableRow extends Component {
   get defaults() {
     return {
+      // @ts-ignore
       ...super.defaults,
       tagName,
       draggable: ['thead', 'tbody', 'tfoot'],
       droppable: ['th', 'td'],
     };
   }
-}
 
-ComponentTableRow.isComponent = el => toLowerCase(el.tagName) === tagName;
+  static isComponent(el: HTMLElement) {
+    return toLowerCase(el.tagName) === tagName;
+  }
+}
