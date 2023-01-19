e2f168f277a098225c5e0ec4b02108d8061a4f24
Artur Arseniev <artur.catch@hotmail.it>
2023-01-19 17:32:53 +0400

    Move ComponentText to TS

--- src/dom_components/model/ComponentText.ts
+++ src/dom_components/model/ComponentText.ts
@@ -3,6 +3,7 @@ import Component from './Component';
 export default class ComponentText extends Component {
   get defaults() {
     return {
+      // @ts-ignore
       ...super.defaults,
       type: 'text',
       droppable: false,
