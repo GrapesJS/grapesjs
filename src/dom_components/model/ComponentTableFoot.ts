bace9b7d2a3bd52f1b5bfa0428c95463767be242
Artur Arseniev <artur.catch@hotmail.it>
2023-01-19 17:30:52 +0400

    Move ComponentTableFoot to TS

--- src/dom_components/model/ComponentTableFoot.ts
+++ src/dom_components/model/ComponentTableFoot.ts
@@ -1,16 +1,19 @@
 import ComponentTableBody from './ComponentTableBody';
-import { toLowerCase } from 'utils/mixins';
+import { toLowerCase } from '../../utils/mixins';
 
 const type = 'tfoot';
 
 export default class ComponentTableFoot extends ComponentTableBody {
   get defaults() {
     return {
+      // @ts-ignore
       ...super.defaults,
       type,
       tagName: type,
     };
   }
-}
 
-ComponentTableFoot.isComponent = el => toLowerCase(el.tagName) === type;
+  static isComponent(el: HTMLElement) {
+    return toLowerCase(el.tagName) === type;
+  }
+}
