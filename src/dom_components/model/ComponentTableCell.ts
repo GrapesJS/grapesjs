b313482371a0d1db07a90e07a93e86ee5f31e248
Artur Arseniev <artur.catch@hotmail.it>
2023-01-19 17:30:00 +0400

    Move ComponentTableCell to TS

--- src/dom_components/model/ComponentTableCell.ts
+++ src/dom_components/model/ComponentTableCell.ts
@@ -1,15 +1,18 @@
 import Component from './Component';
-import { toLowerCase } from 'utils/mixins';
+import { toLowerCase } from '../../utils/mixins';
 
 export default class ComponentTableCell extends Component {
   get defaults() {
     return {
+      // @ts-ignore
       ...super.defaults,
       type: 'cell',
       tagName: 'td',
       draggable: ['tr'],
     };
   }
-}
 
-ComponentTableCell.isComponent = el => ['td', 'th'].indexOf(toLowerCase(el.tagName)) >= 0;
+  static isComponent(el: HTMLElement) {
+    return ['td', 'th'].indexOf(toLowerCase(el.tagName)) >= 0;
+  }
+}
