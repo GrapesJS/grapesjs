3c6c77f8a034955aaaa2ba4d28ba7932600ee3e3
Artur Arseniev <artur.catch@hotmail.it>
2023-01-21 13:27:57 +0400

    Move ComponentCommentView to TS

--- src/dom_components/view/ComponentCommentView.js
+++
@@ -1,7 +0,0 @@
-import ComponentView from './ComponentTextNodeView';
-
-export default class ComponentCommentView extends ComponentView {
-  _createElement() {
-    return document.createComment(this.model.get('content'));
-  }
-}
