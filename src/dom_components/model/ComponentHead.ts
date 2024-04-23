9abacce460dc23cf2db1384cd1a24a5a52f5434e
Artur Arseniev <artur.catch@hotmail.it>
2024-04-23 20:06:43 +0400

    Add ComponentHead

---
+++ src/dom_components/model/ComponentHead.ts
@@ -0,0 +1,21 @@
+import Component from './Component';
+import { toLowerCase } from '../../utils/mixins';
+
+const type = 'head';
+
+export default class ComponentHead extends Component {
+  get defaults() {
+    return {
+      // @ts-ignore
+      ...super.defaults,
+      type,
+      tagName: type,
+      draggable: false,
+      droppable: ['title', 'style', 'base', 'link', 'meta', 'script', 'noscript'],
+    };
+  }
+
+  static isComponent(el: HTMLElement) {
+    return toLowerCase(el.tagName) === type;
+  }
+}
