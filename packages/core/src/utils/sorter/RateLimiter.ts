39e51898c6cb7d26d8e30a34a9f8111ad3ed5eb7
mohamedsalem401 <salemmohamed780@gmail.com>
2024-10-03 21:50:48 +0300

    Format class

--- packages/core/src/utils/sorter/RateLimiter.ts
+++ packages/core/src/utils/sorter/RateLimiter.ts
@@ -1,31 +1,31 @@
 export class RateLimiter<T> {
-    private threshold: number;
-    private lastArgs: T | undefined;
-    private timeout: NodeJS.Timeout | null = null;
+  private threshold: number;
+  private lastArgs: T | undefined;
+  private timeout: NodeJS.Timeout | null = null;
 
-    constructor(threshold: number) {
-        this.threshold = threshold;
-    }
+  constructor(threshold: number) {
+    this.threshold = threshold;
+  }
 
-    updateArgs(args: T) {
-        this.lastArgs = args;
-    }
+  updateArgs(args: T) {
+    this.lastArgs = args;
+  }
 
-    execute(callback: (args: T) => void) {
-        if (!this.timeout) {
-            this.timeout = setTimeout(() => {
-                if (this.lastArgs) {
-                    callback(this.lastArgs);
-                }
-                this.timeout = null;
-            }, this.threshold);
+  execute(callback: (args: T) => void) {
+    if (!this.timeout) {
+      this.timeout = setTimeout(() => {
+        if (this.lastArgs) {
+          callback(this.lastArgs);
         }
+        this.timeout = null;
+      }, this.threshold);
     }
+  }
 
-    clearTimeout() {
-        if (this.timeout) {
-            clearTimeout(this.timeout);
-            this.timeout = null;
-        }
+  clearTimeout() {
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+      this.timeout = null;
     }
+  }
 }
