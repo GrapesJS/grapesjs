a6aa3c1241c5bde0763a15222d98d0472c422b66
danstarns <danielstarns@hotmail.com>
2024-10-09 08:15:03 -0700

    refactor: remove external deps in test

--- packages/core/test/specs/data_sources/jsonplaceholder.ts
+++ packages/core/test/specs/data_sources/jsonplaceholder.ts
@@ -6,17 +6,46 @@ import { setupTestEditor } from '../../common';
 import EditorModel from '../../../src/editor/model/Editor';
 import htmlFormat from 'pretty';
 
-async function getComments() {
-  const url = 'https://jsonplaceholder.typicode.com/posts/1/comments';
-  const response = await fetch(url, {
-    method: 'GET',
-  });
+function getComments() {
+  const json = [
+    {
+      postId: 1,
+      id: 1,
+      name: 'id labore ex et quam laborum',
+      email: 'Eliseo@gardner.biz',
+      body: 'laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium',
+    },
+    {
+      postId: 1,
+      id: 2,
+      name: 'quo vero reiciendis velit similique earum',
+      email: 'Jayne_Kuhic@sydney.com',
+      body: 'est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et',
+    },
+    {
+      postId: 1,
+      id: 3,
+      name: 'odio adipisci rerum aut animi',
+      email: 'Nikita@garfield.biz',
+      body: 'quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione',
+    },
+    {
+      postId: 1,
+      id: 4,
+      name: 'alias odio sit',
+      email: 'Lew@alysha.tv',
+      body: 'non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati',
+    },
+    {
+      postId: 1,
+      id: 5,
+      name: 'vero eaque aliquid doloribus et culpa',
+      email: 'Hayden@althea.biz',
+      body: 'harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et',
+    },
+  ];
 
-  if (!response.ok) {
-    throw new Error('Failed to fetch comments');
-  }
-
-  return response.json();
+  return json;
 }
 
 // Comment https://github.com/GrapesJS/grapesjs/discussions/5956#discussioncomment-10559499
@@ -34,10 +63,10 @@ describe('JsonPlaceholder Usage', () => {
   });
 
   test('should render a list of comments from jsonplaceholder api', async () => {
-    const comments = await getComments();
+    const comments = getComments();
     const dataSource: DataSourceProps = {
       id: 'comments',
-      records: comments,
+      records: comments as any,
     };
     dsm.add(dataSource);
 
