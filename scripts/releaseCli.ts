c37139a20bf0ec6786985cfdf442d75eba05e5c2
Artur Arseniev <artur.catch@hotmail.it>
2024-10-21 11:35:52 +0400

    Update release core files

--- scripts/releaseCli.ts
+++ scripts/releaseCli.ts
@@ -2,11 +2,11 @@ import fs from 'fs';
 import { resolve } from 'path';
 import { runCommand } from './common';
 
-const sdkPath = resolve(__dirname, '../packages/cli');
+const pathLib = resolve(__dirname, '../packages/cli');
 
 async function prepareCoreRelease() {
   try {
-    const releaseTag = process.argv[2];
+    const releaseTag = process.argv[2] || 'rc';
     console.log('Prepare release cli tag:', releaseTag);
 
     // Check if the current branch is clean (no staged changes)
@@ -17,10 +17,10 @@ async function prepareCoreRelease() {
 
     // Increment the CLI version
     const versionCmd = releaseTag === 'latest' ? '--patch' : `--prerelease --preid ${releaseTag}`;
-    // runCommand(`yarn workspace @grapesjs/studio-sdk version ${versionCmd} --no-git-tag-version --no-commit-hooks`);
+    runCommand(`pnpm --filter grapesjs-cli version ${versionCmd} --no-git-tag-version --no-commit-hooks`);
 
     // Create a new release branch
-    const newVersion = JSON.parse(fs.readFileSync(`${sdkPath}/package.json`, 'utf8')).version;
+    const newVersion = JSON.parse(fs.readFileSync(`${pathLib}/package.json`, 'utf8')).version;
     const newBranch = `release-v${newVersion}`;
     runCommand(`git checkout -b ${newBranch}`);
     runCommand('git add .');
--- scripts/releaseCore.ts
+++ scripts/releaseCore.ts
@@ -2,11 +2,11 @@ import fs from 'fs';
 import { resolve } from 'path';
 import { runCommand } from './common';
 
-const sdkPath = resolve(__dirname, '../packages/core');
+const pathLib = resolve(__dirname, '../packages/core');
 
 async function prepareCoreRelease() {
   try {
-    const releaseTag = process.argv[2];
+    const releaseTag = process.argv[2] || 'rc';
     console.log('Prepare release core tag:', releaseTag);
 
     // Check if the current branch is clean (no staged changes)
@@ -17,10 +17,10 @@ async function prepareCoreRelease() {
 
     // Increment the Core version
     const versionCmd = releaseTag === 'latest' ? '--patch' : `--prerelease --preid ${releaseTag}`;
-    // runCommand(`yarn workspace @grapesjs/studio-sdk version ${versionCmd} --no-git-tag-version --no-commit-hooks`);
+    runCommand(`pnpm --filter grapesjs version ${versionCmd} --no-git-tag-version --no-commit-hooks`);
 
     // Create a new release branch
-    const newVersion = JSON.parse(fs.readFileSync(`${sdkPath}/package.json`, 'utf8')).version;
+    const newVersion = JSON.parse(fs.readFileSync(`${pathLib}/package.json`, 'utf8')).version;
     const newBranch = `release-v${newVersion}`;
     runCommand(`git checkout -b ${newBranch}`);
     runCommand('git add .');
