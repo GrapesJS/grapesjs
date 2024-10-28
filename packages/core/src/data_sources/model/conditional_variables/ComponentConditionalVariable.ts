8f2a75656e5cbe817dff912c83e40477f40662c4
mohamedsalem401 <salemmohamed780@gmail.com>
2024-10-28 17:57:14 +0300

    Simplify component conditional variable

--- packages/core/src/data_sources/model/conditional_variables/ComponentConditionalVariable.ts
+++ packages/core/src/data_sources/model/conditional_variables/ComponentConditionalVariable.ts
@@ -5,12 +5,10 @@ import { Condition } from './Condition';
 import { DataConditionType } from './DataCondition';
 
 export default class ComponentConditionalVariable extends Component {
-  condition?: Condition;
-
   getDataValue(): any {
-    const { condition, ifTrue, ifFalse, em } = this.attributes;
-    this.condition = new Condition(condition, { em });
-    return this.condition.evaluate() ? evaluateVariable(ifTrue, em) : evaluateVariable(ifFalse, em);
+    const { condition: conditionAttribute, ifTrue, ifFalse, em } = this.attributes;
+    const condition = new Condition(conditionAttribute, { em });
+    return condition.evaluate() ? evaluateVariable(ifTrue, em) : evaluateVariable(ifFalse, em);
   }
 
   getInnerHTML() {
