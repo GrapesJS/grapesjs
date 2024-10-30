80baf2cba2d5d094ca3959679873096d79870100
mohamedsalem401 <salemmohamed780@gmail.com>
2024-10-30 12:52:23 +0300

    Add tests for conditional traits

---
+++ packages/core/test/specs/data_sources/model/conditional_variables/ConditionalTraits.ts
@@ -0,0 +1,174 @@
+import { DataSourceManager, Editor } from '../../../../../src';
+import { DataVariableType } from '../../../../../src/data_sources/model/DataVariable';
+import { MissingConditionError } from '../../../../../src/data_sources/model/conditional_variables/DataCondition';
+import { ConditionalVariableType } from '../../../../../src/data_sources/model/conditional_variables/DataCondition';
+import { GenericOperation } from '../../../../../src/data_sources/model/conditional_variables/operators/GenericOperator';
+import { NumberOperation } from '../../../../../src/data_sources/model/conditional_variables/operators/NumberOperator';
+import { DataSourceProps } from '../../../../../src/data_sources/types';
+import ComponentWrapper from '../../../../../src/dom_components/model/ComponentWrapper';
+import EditorModel from '../../../../../src/editor/model/Editor';
+import { setupTestEditor } from '../../../../common';
+
+describe('TraitConditionalVariable', () => {
+  let editor: Editor;
+  let em: EditorModel;
+  let dsm: DataSourceManager;
+  let cmpRoot: ComponentWrapper;
+
+  beforeEach(() => {
+    ({ editor, em, dsm, cmpRoot } = setupTestEditor());
+  });
+
+  afterEach(() => {
+    em.destroy();
+  });
+
+  it('should add a trait with a condition evaluating to a string', () => {
+    const component = cmpRoot.append({
+      tagName: 'h1',
+      type: 'text',
+      traits: [
+        {
+          type: 'text',
+          name: 'title',
+          value: {
+            type: ConditionalVariableType,
+            condition: {
+              left: 0,
+              operator: NumberOperation.greaterThan,
+              right: -1,
+            },
+            ifTrue: 'Some title',
+          },
+        },
+      ],
+    })[0];
+
+    expect(component).toBeDefined();
+    expect(component.getTrait('title').get('value')).toBe('Some title');
+  });
+
+  it('should add a trait with a data-source condition', () => {
+    const dataSource: DataSourceProps = {
+      id: 'ds1',
+      records: [{ id: 'left_id', left: 'Name1' }],
+    };
+    dsm.add(dataSource);
+
+    const component = cmpRoot.append({
+      tagName: 'h1',
+      type: 'text',
+      traits: [
+        {
+          type: 'text',
+          name: 'value',
+          value: {
+            type: ConditionalVariableType,
+            condition: {
+              left: {
+                type: DataVariableType,
+                path: 'ds1.left_id.left',
+              },
+              operator: GenericOperation.equals,
+              right: 'Name1',
+            },
+            ifTrue: 'Valid name',
+            ifFalse: 'Invalid name',
+          },
+        },
+      ],
+    })[0];
+
+    const traitValue = component.getTrait('value').get('value');
+    expect(traitValue).toBe('Valid name');
+  });
+
+  it('should change trait value with changing data-source value', () => {
+    const dataSource: DataSourceProps = {
+      id: 'ds1',
+      records: [{ id: 'left_id', left: 'Name1' }],
+    };
+    dsm.add(dataSource);
+
+    const component = cmpRoot.append({
+      tagName: 'h1',
+      type: 'text',
+      traits: [
+        {
+          type: 'text',
+          name: 'value',
+          value: {
+            type: ConditionalVariableType,
+            condition: {
+              left: {
+                type: DataVariableType,
+                path: 'ds1.left_id.left',
+              },
+              operator: GenericOperation.equals,
+              right: 'Name1',
+            },
+            ifTrue: 'Correct name',
+            ifFalse: 'Incorrect name',
+          },
+        },
+      ],
+    })[0];
+
+    const traitValueBefore = component.getTrait('value').get('value');
+    expect(traitValueBefore).toBe('Correct name');
+
+    dsm.get('ds1').getRecord('left_id')?.set('left', 'Different name');
+    const traitValueAfter = component.getTrait('value').get('value');
+    expect(traitValueAfter).toBe('Incorrect name');
+  });
+
+  it('should throw an error if no condition is passed in trait', () => {
+    expect(() => {
+      cmpRoot.append({
+        tagName: 'h1',
+        type: 'text',
+        traits: [
+          {
+            type: 'text',
+            name: 'invalidTrait',
+            value: {
+              type: ConditionalVariableType,
+            },
+          },
+        ],
+      });
+    }).toThrow(MissingConditionError);
+  });
+
+  it('should store traits with conditional values correctly', () => {
+    const conditionalTrait = {
+      type: ConditionalVariableType,
+      condition: {
+        left: 0,
+        operator: NumberOperation.greaterThan,
+        right: -1,
+      },
+      ifTrue: 'Positive',
+    };
+    cmpRoot.append({
+      tagName: 'h1',
+      type: 'text',
+      traits: [
+        {
+          type: 'text',
+          name: 'dynamicTrait',
+          value: conditionalTrait,
+        },
+      ],
+    })[0];
+
+    const projectData = editor.getProjectData();
+    const page = projectData.pages[0];
+    const frame = page.frames[0];
+    const storedComponent = frame.component.components[0];
+
+    expect(storedComponent['attributes-dynamic-value']).toEqual({
+      dynamicTrait: conditionalTrait,
+    });
+  });
+});
