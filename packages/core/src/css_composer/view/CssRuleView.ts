7ae381c230687f7b78431fb6fbaa6f954d068353
mohamedsalem401 <salemmohamed780@gmail.com>
2025-10-08 12:00:49 +0300

    make animation for cssRuleView

--- packages/core/src/css_composer/view/CssRuleView.ts
+++ packages/core/src/css_composer/view/CssRuleView.ts
@@ -6,14 +6,19 @@ import { CssEvents } from '../types';
 
 export default class CssRuleView extends View<CssRule> {
   config: any;
+  private cachedCss = '';
+  private rafId: number | null = null;
+  private isDirty = false;
 
   constructor(o: any = {}) {
     super(o);
     this.config = o.config || {};
     const { model } = this;
-    this.listenTo(model, 'change', this.render);
+
+    this.listenTo(model, 'change', this.requestRender);
     this.listenTo(model, 'destroy remove', this.remove);
-    this.listenTo(model.get('selectors'), 'change', this.render);
+    this.listenTo(model.get('selectors'), 'change', this.requestRender);
+
     model.setView(this);
   }
 
@@ -26,13 +31,10 @@ export default class CssRuleView extends View<CssRule> {
   }
 
   remove() {
+    if (this.rafId) cancelAnimationFrame(this.rafId);
     super.remove();
     this.model.removeView(this);
     return this;
-  }
-
-  updateStyles() {
-    this.render();
   }
 
   /** @ts-ignore */
@@ -40,13 +42,33 @@ export default class CssRuleView extends View<CssRule> {
     return 'style';
   }
 
+  private requestRender = () => {
+    if (this.isDirty) return;
+    this.isDirty = true;
+
+    this.rafId = requestAnimationFrame(() => {
+      this.isDirty = false;
+      this.render();
+    });
+  };
+
+  updateStyles() {
+    this.requestRender();
+  }
+
   render() {
     const { model, el, em } = this;
     const important = model.get('important');
     const css = model.toCSS({ important });
+
+    if (css === this.cachedCss) return this;
+    this.cachedCss = css;
+
     const mountProps = { rule: model, ruleView: this, css };
     em?.trigger(CssEvents.mountBefore, mountProps);
-    el.innerHTML = mountProps.css;
+
+    el.textContent = mountProps.css;
+
     em?.trigger(CssEvents.mount, mountProps);
     return this;
   }
