bfd98e5714e32e5c2bc708cb6ad6e033641a9f57
IhorKaleniuk666 <ihor.kaleniuk@newgen.company>
2026-01-20 14:19:25 +0200

    fix lint

--- packages/core/src/asset_manager/model/Assets.ts
+++ packages/core/src/asset_manager/model/Assets.ts
@@ -5,24 +5,21 @@ import AssetImageView from '../view/AssetImageView';
 import TypeableCollection from '../../domain_abstract/model/TypeableCollection';
 
 export class Assets extends CollectionWithPatches<Asset> {
+  types: any[] | undefined;
+  target?: any;
+  onSelect?: any;
+  getTypes!: () => any[];
+  getType!: (id: string) => any;
+  getBaseType!: () => any;
+  recognizeType!: (value: any) => any;
+  addType!: (id: string, definition: any) => void;
+
   constructor(models?: any, options?: any) {
     super(models, options);
   }
 }
 
-export interface Assets {
-  types: any[];
-  target?: any;
-  onSelect?: any;
-  getTypes(): any[];
-  getType(id: string): any;
-  getBaseType(): any;
-  recognizeType(value: any): any;
-  addType(id: string, definition: any): void;
-}
-
 Object.assign(Assets.prototype, TypeableCollection);
-
 Assets.prototype.types = [
   {
     id: 'image',
